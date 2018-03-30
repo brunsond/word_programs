11e834f208c52ccaac8158224a9b4d040e5dd74d
brunsond <37097298+brunsond@users.noreply.github.com>
2018-03-29 19:06:15 -0600

    Delete word_count.py

--- word_count.py
+++
@@ -1,9 +0,0 @@
-filename = raw_input("Name of file: ")
-textpage = open(filename,'r')
-count = 0
-for line in textpage:
-    count += len(line.split())
-
-print "Number of words: {}".format(count)
-
-
